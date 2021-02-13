15801adcd36c54d2a937fc71de6110cfc3f40e35
Anurag <anuragarwalkar@gmail.com>
2021-02-14 02:32:40 +0530

    added skip install flag

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-const util = require("util");
-const path = require("path");
-const fs = require("fs");
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
 
 // Unility Functions
-const exec = util.promisify(require("child_process").exec);
+const exec = util.promisify(require('child_process').exec);
 async function exeCommand(command) {
   try {
     const { stdout, stderr } = await exec(command);
@@ -16,26 +16,37 @@ async function exeCommand(command) {
   }
 }
 
-// Validate arguments
-if (process.argv.length < 3) {
-  console.log("Please enter a valid project name");
-  console.log("npx create-nodejs-typescript-app {projectName}");
-  process.exit(1);
+function processInput() {
+  // Validate arguments
+  if (process.argv.length < 3) {
+    console.log('Please enter a valid project name');
+    console.log('npx create-nodejs-typescript-app {projectName}');
+    process.exit(1);
+  }
+
+  const object = {};
+  const argvLength = process.argv.length;
+  for (let i = 1; i < argvLength; i++) {
+    if (process.argv[i].startsWith('--skip-install')) {
+      object.skipInstall = true;
+    } else {
+      object.folderName = process.argv[i];
+    }
+  }
+
+  return object;
 }
 
 const basePath = process.cwd();
-const folderName = process.argv[2];
+const { folderName, skipInstall = false } = processInput();
 const appPath = path.join(basePath, folderName);
-const repo =
-  "https://github.com/anuragarwalkar/node-express-typescript-boilerplate.git";
+const repo = 'https://github.com/anuragarwalkar/node-express-typescript-boilerplate.git';
 
 try {
   fs.mkdirSync(appPath);
 } catch (err) {
-  if (err.code === "EEXIST") {
-    console.log(
-      "Directory already exists. Please choose another name for the project."
-    );
+  if (err.code === 'EEXIST') {
+    console.log('Directory already exists. Please choose another name for the project.');
   } else {
     console.log(error);
   }
@@ -51,8 +62,7 @@ function removeFiles(files) {
 async function cloneRepo() {
   console.log(`Downloading files from repo ${repo}`);
   await exeCommand(`git clone --depth 1 ${repo} ${folderName}`);
-  console.log("GitHub repository cloned successfully.");
-  console.log("");
+  console.log('GitHub repository cloned successfully.');
 }
 
 async function installDependencies() {
@@ -60,18 +70,14 @@ async function installDependencies() {
   process.chdir(appPath);
 
   // Install dependencies
-  console.log("Installing the dependencies...");
-  await exeCommand("npm install");
-  console.log("Dependencies has been installed successfully.");
-  console.log("");
+  console.log('Installing the dependencies 🚀🚀🚀');
+  await exeCommand('npm install');
+  console.log('Dependencies has been installed successfully.');
 }
 
 function installComplete() {
-  console.log("Installation is now complete!");
-  console.log("");
-
-  console.log("We suggest that you start by typing:");
-  console.log(`    cd ${folderName}`);
+  console.log('Installation is now complete! 🥂');
+  console.log(`cd ${folderName}`);
 }
 
 async function main() {
@@ -79,13 +85,15 @@ async function main() {
   await cloneRepo();
 
   // Install Dependencies
-  await installDependencies();
+  if (!skipInstall) {
+    await installDependencies();
+  }
 
   // Delete .git folder
-  await exeCommand("npx rimraf ./.git");
+  await exeCommand('npx rimraf ./.git');
 
   // Remove development files
-  const files = ["index.js", "README.md"];
+  const files = ['index.js'];
   removeFiles(files);
 
   // Install complete message
