049d166f54ad0081c77b1aaadca3999bcdc27f3f
Anurag Arwalkar <anuragarwalkar@gmail.com>
2021-02-26 11:33:38 +0530

    mongodb connection fix

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,14 @@ import config from './config/config';
 import app from './app';
 import logger from './config/logger';
 
-mongoose.connect(config.mongoose.url, config.mongoose.options, () => {
-  logger.info('Connected to MongoDB');
-});
+(async () => {
+  try {
+    await mongoose.connect(config.mongoose.url, config.mongoose.options);
+    logger.info('Connected to MongoDB');
+  } catch (error) {
+    logger.error(error);
+  }
+})();
 
 app.listen(config.port, () => {
   logger.info(`Running on http://${config.host}:${config.port}`);
