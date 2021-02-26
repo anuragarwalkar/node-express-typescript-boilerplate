8efbc51595a4f9748c87d3608d64e2eacf0dd833
Anurag Arwalkar <anuragarwalkar@gmail.com>
2021-02-26 11:55:30 +0530

    added basic swagger documentation for demo route

--- src/routes/v1/demo.routes.ts
+++ src/routes/v1/demo.routes.ts
@@ -3,6 +3,16 @@ import { demo } from '../../controller/demo.controller';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /demo:
+ *   get:
+ *     description: Returns the hello world message
+ *     responses:
+ *       200:
+ *         description: hello world
+ */
+
 // You can append .post, .put, .patch etc method to this route
 // more info https://expressjs.com/en/guide/routing.html
 router.route('/').get(demo);
